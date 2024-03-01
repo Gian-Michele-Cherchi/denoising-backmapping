45e7728469f8563283736ee884b30c75c7bf3c3d
Gian-Michele <giovanni_michel.cherchi@uca.fr>
2024-03-01 12:47:30 +0100

    score losses script add

---
+++ src/model/losses.py
@@ -0,0 +1,159 @@
+# coding=utf-8
+# Copyright 2020 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""All functions related to loss computation and optimization.
+"""
+
+import torch
+import torch.optim as optim
+import numpy as np
+from utils.misc import *
+from model.sde import VPSDE
+
+
+def get_sde_loss_fn(sde, train, reduce_mean=True, continuous=True, likelihood_weighting=True, eps=1e-5):
+    """Create a loss function for training with arbirary SDEs.
+
+    Args:
+        sde: An `sde_lib.SDE` object that represents the forward SDE.
+        train: `True` for training loss and `False` for evaluation loss.
+        reduce_mean: If `True`, average the loss across data dimensions. Otherwise sum the loss across data dimensions.
+        continuous: `True` indicates that the model is defined to take continuous time steps. Otherwise it requires
+        ad-hoc interpolation to take continuous time steps.
+        likelihood_weighting: If `True`, weight the mixture of score matching losses
+        according to https://arxiv.org/abs/2101.09258; otherwise use the weighting recommended in our paper.
+        eps: A `float` number. The smallest time step to sample from.
+
+    Returns:
+        A loss function.
+    """
+    reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
+    
+    def loss_fn(model, batch):
+        """Compute the loss function.
+
+        Args:
+        model: A score model.
+        batch: A mini-batch of training data.
+
+        Returns:
+        loss: A scalar that represents the average loss value across the mini-batch.
+        """
+        score_fn = get_score_fn(sde, model, train=train, continuous=continuous)
+        t = torch.rand(batch.shape[0], device=batch.device) * (sde.T - eps) + eps
+        z = torch.randn_like(batch)
+        mean, std = sde.marginal_prob(batch, t)
+        perturbed_dist = mean + std[:, None] * z 
+        score = score_fn(perturbed_dist, t)
+        
+        if not likelihood_weighting:
+            losses = torch.square(score * std[:, None] + z )
+            losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
+        else:
+            g2 = sde.sde(torch.zeros_like(batch), t)[1] ** 2
+            losses = torch.square(score + z / std[:, None])
+            losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1) * g2
+      
+        loss = torch.mean(losses)
+        return loss
+
+    return loss_fn
+        
+
+def get_ddpm_loss_fn(vpsde, train, reduce_mean=True):
+  """Legacy code to reproduce previous results on DDPM. Not recommended for new work."""
+  assert isinstance(vpsde, VPSDE), "DDPM training only works for VPSDEs."
+
+  reduce_op = torch.mean if reduce_mean else lambda *args, **kwargs: 0.5 * torch.sum(*args, **kwargs)
+
+  def loss_fn(model, batch):
+    model_fn = get_model_fn(model, train=train)
+    labels = torch.randint(0, vpsde.N, (batch.shape[0],), device=batch.device)
+    sqrt_alphas_cumprod = vpsde.sqrt_alphas_cumprod.to(batch.device)
+    sqrt_1m_alphas_cumprod = vpsde.sqrt_1m_alphas_cumprod.to(batch.device)
+    noise = torch.randn_like(batch)
+    perturbed_data = sqrt_alphas_cumprod[labels, None] * batch + \
+                     sqrt_1m_alphas_cumprod[labels, None] * noise
+    score = model_fn(perturbed_data, labels)
+    losses = torch.square(score - noise)
+    losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
+    loss = torch.mean(losses)
+    return loss
+
+  return loss_fn
+
+
+
+
+def get_step_fn(sde, train, optimize_fn=None, reduce_mean=False, continuous=True, likelihood_weighting=False):
+  """Create a one-step training/evaluation function.
+
+  Args:
+    sde: An `sde_lib.SDE` object that represents the forward SDE.
+    optimize_fn: An optimization function.
+    reduce_mean: If `True`, average the loss across data dimensions. Otherwise sum the loss across data dimensions.
+    continuous: `True` indicates that the model is defined to take continuous time steps.
+    likelihood_weighting: If `True`, weight the mixture of score matching losses according to
+      https://arxiv.org/abs/2101.09258; otherwise use the weighting recommended by our paper.
+
+  Returns:
+    A one-step function for training or evaluation.
+  """
+  if continuous:
+    loss_fn = get_sde_loss_fn(sde, train, reduce_mean=reduce_mean,
+                              continuous=True, likelihood_weighting=likelihood_weighting)
+  else:
+    assert not likelihood_weighting, "Likelihood weighting is not supported for original SMLD/DDPM training."
+    
+    if isinstance(sde, VPSDE):
+      loss_fn = get_ddpm_loss_fn(sde, train, reduce_mean=reduce_mean)
+    else:
+      raise ValueError(f"Discrete training for {sde.__class__.__name__} is not recommended.")
+  
+  
+  def step_fn(state, batch):
+    """Running one step of training or evaluation.
+
+    This function will undergo `jax.lax.scan` so that multiple steps can be pmapped and jit-compiled together
+    for faster execution.
+
+    Args:
+      state: A dictionary of training information, containing the score model, optimizer,
+       EMA status, and number of optimization steps.
+      batch: A mini-batch of training/evaluation data.
+
+    Returns:
+      loss: The average loss value of this state.
+    """
+    model = state['model']
+    if train:
+      optimizer = state['optimizer']
+      optimizer.zero_grad()
+      loss = loss_fn(model, batch)
+      loss.backward()
+      optimize_fn(optimizer, model.parameters(), step=state['step'])
+      state['step'] += 1
+      state['ema'].update(model.parameters())
+    else:
+      with torch.no_grad():
+        ema = state['ema']
+        ema.store(model.parameters())
+        ema.copy_to(model.parameters())
+        loss = loss_fn(model, batch)
+        ema.restore(model.parameters())
+
+    return loss
+
+  return step_fn
