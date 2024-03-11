3397ebea53763aece2ab56f505549d194a784aa7
Gian-Michele <giovanni_michel.cherchi@uca.fr>
2024-03-11 16:28:28 +0100

    coarse grain and reverse coarse grain att add to polymer dataset class

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,7 +1,7 @@
 import os.path as osp
 import os 
 from multiprocessing import Pool
-
+from collections import Counter
 from rdkit import Chem
 import numpy as np 
 import torch
@@ -12,7 +12,7 @@ from torch_geometric.data import Dataset, Data
 from torch_geometric.loader import DataLoader
 from data.featurization import featurize_pol_from_smiles
 TYPES = {'H': 0, 'C': 1}
-
+ATOMIC_NUMBERS = {'H': 1, 'C': 6}
 __DATASET__ = {}
 
 def register_dataset(name: str):
@@ -30,9 +30,10 @@ def get_dataset(name: str, path: str,  **kwargs):
 
 @register_dataset('polymer_melt')
 class PolymerMeltDataset(Dataset):
-    def __init__(self,  path , mode,  save_ckpt:bool=True, device: str='cpu'):
+    def __init__(self,  path , mode, coarse_grain: bool=True ,  save_ckpt:bool=True, device: str='cpu'):
         super(PolymerMeltDataset, self).__init__()
         self.device = device
+        self.cg = coarse_grain
         self.path = path
         try: 
             self.datapoints = torch.load(path+'/datapoints.pt', map_location=device)
@@ -62,39 +63,46 @@ class PolymerMeltDataset(Dataset):
             data = self.read_lammpstraj(osp.join(path, ds, 'dump.lammpstrj'))
             dataset.append(data)
             types.append(data['types'])
-            smiles.append(r"C\C=C/C" * data['n_monomers'].max())
-            
+            smiles.append(r"C\C=C/C" * self.n_monomers)
+        
         self.smiles = set(smiles)
         self.types = set(types[0])
-        confs = self.features_from_smiles(dataset[0])
+        dataset = self.coarse_grain(dataset[0])
+        confs = self.features_from_smiles(dataset)
         return confs
     
     
     def features_from_smiles(self, dataset):
         
         self.mol_features= [featurize_pol_from_smiles(smile, types=self.types, hydrogens=True) for smile in self.smiles][0]
-        atom_in_mon = 102
-        n_molecules = dataset["pos"].size(1) // atom_in_mon
         n_edges = self.mol_features[0].edge_index.size(1)
-        match_order = dataset["atom_types"][:atom_in_mon]
+        match_order = dataset["atom_types"][:self.atom_in_mol]
         indexes_res = [i for i, x in enumerate(match_order) if x != 'H']
         indexes_hyd = [i for i, x in enumerate(match_order) if x == 'H']
-        reindex = indexes_res + indexes_hyd
-        ext_edge_index = self.mol_features[0].edge_index.repeat(1,n_molecules)
+        self.reindex = indexes_res + indexes_hyd
+        ext_edge_index = self.mol_features[0].edge_index.repeat(1,self.n_molecules)
         const = 0 
-        assert len(reindex) == atom_in_mon
-        for i in range(n_molecules):
-            dataset["pos"][:,i*atom_in_mon:(i+1)*atom_in_mon] = dataset["pos"][:,i*atom_in_mon:(i+1)*atom_in_mon][:,reindex]
+        assert len(self.reindex) == self.atom_in_mol
+        for i in range(self.n_molecules):
+            dataset["pos"][:,i*self.atom_in_mol:(i+1)*self.atom_in_mol] = dataset["pos"][:,i*self.atom_in_mol:(i+1)*self.atom_in_mol][:,self.reindex]
+            dataset["mol"][i*self.atom_in_mol:(i+1)*self.atom_in_mol] = dataset["mol"][i*self.atom_in_mol:(i+1)*self.atom_in_mol][self.reindex]
+            dataset["atom_types"][i*self.atom_in_mol:(i+1)*self.atom_in_mol] = dataset["atom_types"][i*self.atom_in_mol:(i+1)*self.atom_in_mol][self.reindex]
+            dataset["cg_dist"][:,i*self.atom_in_mol:(i+1)*self.atom_in_mol] = dataset["cg_dist"][:,i*self.atom_in_mol:(i+1)*self.atom_in_mol][:,self.reindex]
+            #self.com_matrix[i*self.n_monomers:(i+1)*self.n_monomers] = self.com_matrix[i*self.n_monomers:(i+1)*self.n_monomers][:,self.reindex]
             ext_edge_index[:,i*n_edges:(i+1)*n_edges] = const + ext_edge_index[:,i*n_edges:(i+1)*n_edges]
-            const += atom_in_mon
+            const += self.atom_in_mol
         graph_inst = {}
         for index,conf in enumerate(dataset["pos"]):
-            graph_conf = Data(x=self.mol_features[0].x.repeat(n_molecules, 1),
+            graph_conf = Data(x=self.mol_features[0].x.repeat(self.n_molecules, 1),
                               edge_index=ext_edge_index, 
-                              edge_attr=self.mol_features[0].edge_attr.repeat(n_molecules, 1), 
-                              z=self.mol_features[0].z.repeat(n_molecules))
+                              edge_attr=self.mol_features[0].edge_attr.repeat(self.n_molecules, 1), 
+                              z=self.mol_features[0].z.repeat(self.n_molecules))
             graph_conf.pos = conf
+            graph_conf.mol = dataset["mol"]
+            graph_conf.cg_dist = dataset["cg_dist"][index]
+            graph_conf.cg_pos = dataset["cg_pos"][index]
             graph_inst["conf"+str(index)] = graph_conf
+            
         return graph_inst
         
     def read_lammpstraj(self, filename):
@@ -102,16 +110,18 @@ class PolymerMeltDataset(Dataset):
             lines = file.readlines()
         timestep_indices = [i for i, line in enumerate(lines) if line.startswith('ITEM: TIMESTEP')]
         n_atoms_indices = [i for i, line in enumerate(lines) if line.startswith('ITEM: NUMBER OF ATOMS')]
-        n_atoms = int(lines[n_atoms_indices[0]+1].strip().split('\n')[0])
+        self.n_atoms = int(lines[n_atoms_indices[0]+1].strip().split('\n')[0])
         header = lines[timestep_indices[0]+8].strip().split(' ')[2:]
         skip_index = np.array([list(range(i,i+9)) for i in timestep_indices]).reshape(-1)
         df = pd.read_csv(filename, skiprows=skip_index, sep=' ', names=header)
-        n_monomers = df.mol.unique()
+        self.n_monomers = df.mol.unique().max()
         types  = list(df.element.unique())
-        n_conf = len(df) // n_atoms
-        pos = torch.Tensor(df[['xu', 'yu', 'zu']].values)[None,...].view(n_conf,n_atoms, 3)
-        mol = torch.Tensor(df["mol"].values)[None,...].view(n_conf,n_atoms)[0]
+        n_conf = len(df) // self.n_atoms
+        pos = torch.Tensor(df[['xu', 'yu', 'zu']].values)[None,...].view(n_conf,self.n_atoms, 3)
+        mol = torch.Tensor(df["mol"].values)[None,...].view(n_conf,self.n_atoms)[0]
         atom_type = df['element'].values[:skip_index[9]-9]
+        self.atom_in_mol = 102
+        self.n_molecules = pos.size(1) // self.atom_in_mol
         assert len(atom_type) == mol.size(0)
         del df
             
@@ -119,12 +129,68 @@ class PolymerMeltDataset(Dataset):
             "mol":mol,
             "types":types,
             "atom_types":atom_type,
-            "n_monomers":n_monomers}
+            }
         
-    def coarse_grain(self, ):
-        
-        for conf in batch:
-            conf = self.smiles_to_graph(conf)
+    def coarse_grain(self, dataset):
+        atom_monomer_id = dataset["mol"]# [n_atoms x 1], values from 1 to n_monomers 
+        self.com_matrix = torch.zeros(self.n_molecules * self.n_monomers, self.n_atoms, device=self.device)
+        partial_relative_mass = torch.tensor([ATOMIC_NUMBERS[dataset["atom_types"][i]] for i in range(len(dataset["atom_types"]))])
+        for i in range(self.n_monomers*self.n_molecules):
+            mon_id = i % self.n_monomers
+            pol_id = i // self.n_monomers
+            index = atom_monomer_id == mon_id+1
+            select_index = torch.arange(self.n_molecules) != pol_id
+            index = index.reshape(self.n_molecules, self.atom_in_mol)
+            index[select_index] = False
+            index = index.reshape(self.n_molecules* self.atom_in_mol)
+            total_monomer_mass = torch.tensor(list(Counter(dataset["atom_types"][index]).values())) @ torch.tensor([1,6])
+            self.com_matrix[i,:] = (partial_relative_mass / total_monomer_mass) * index 
+
+        com_confs = torch.bmm(self.com_matrix[None,...].expand(dataset["pos"].size(0),-1,-1), dataset["pos"])
+        dataset["cg_pos"] = com_confs
+        dist = torch.zeros(dataset["pos"].size(0), self.n_molecules*self.atom_in_mol,3)
+        ################################
+        for i in range(self.n_monomers*self.n_molecules):
+            mon_id = i % self.n_monomers
+            pol_id = i // self.n_monomers
+            index = atom_monomer_id == mon_id+1
+            select_index = torch.arange(self.n_molecules) != pol_id
+            index = index.reshape(self.n_molecules, self.atom_in_mol)
+            index[select_index] = False
+            index = index.reshape(self.n_molecules* self.atom_in_mol)
+            dist[:,index] = dataset["pos"][:,index] -dataset["cg_pos"][:,i:i+1].repeat(1,index.sum().item(),1)
+        dataset["cg_dist"] = dist
+        return dataset
+    
+    def reverse_coarse_grain(self,batch, batch_size: int):
+        """
+        Reverse the coarse graining
+        """
+        n_atom_in_mol = 102
+        n_atoms = batch.pos.size(0) // batch_size
+        n_molecules = n_atoms  // n_atom_in_mol
+        n_monomers = batch.mol.max().item()
+        pos = batch.pos.view(batch_size, n_atoms, -1) 
+        cg_perb_dist = batch.cg_perb_dist.view(batch_size, n_atoms, -1)
+        cg_confs =  batch.cg_pos.view(batch_size, int(n_molecules*n_monomers), -1)
+        atom_monomer_id = batch.mol[batch.batch == 0]
+        # batch [batch_size, n_atoms, 3]
+        batch.perb_pos = pos 
+        for i in range(int(n_monomers*n_molecules)):
+            mon_id = i % n_monomers
+            pol_id = i // n_monomers
+            index = atom_monomer_id == mon_id+1
+            select_index = torch.arange(n_molecules) != pol_id
+            index = index.reshape(n_molecules, n_atom_in_mol)
+            index[select_index] = False
+            index = index.reshape(int(n_molecules* n_atom_in_mol))
+            batch.perb_pos[:,index] = cg_confs[:,i:i+1].repeat(1,index.sum().item(),1) + cg_perb_dist[:,index]
+        batch.perb_pos = batch.perb_pos.reshape(-1,3)
+        return batch
+    
+    
+    def get_com_matrix(self):
+        return self.com_matrix
     
     def get(self, idx: int):
         data = self.datapoints[idx]
@@ -157,7 +223,7 @@ if __name__ == "__main__":
     import argparse 
     args = {"dataset_path":"dataset", "num_workers":4, "batch_size":32, "device": 'cuda:0'}
     args = argparse.Namespace(**args)
-    loaders, dataset = get_dataloader(args)
+    loaders, dataset = get_dataloader(args, batch_size=args.batch_size)
     for batch in loaders["train"]:
         print(batch.to(args.device))
         batch_cg = dataset.coarse_grain(batch.to(args.device))
