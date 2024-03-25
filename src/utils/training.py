cdd36f64b40a1d3681de3f721e470405c73350ae
Gian-Michele <giovanni_michel.cherchi@uca.fr>
2024-03-25 22:09:11 +0100

    train and test function

--- src/utils/training.py
+++ src/utils/training.py
@@ -1,46 +1,35 @@
 import numpy as np
 from tqdm import tqdm
 import torch
-#import diffusion.torus as torus
+from pickle import dump
 
 
 def train_epoch(loss_fn, model, loader, optimizer, device):
     loss_tot = 0
-    base_tot = 0
     model.train()
     for data in tqdm(loader, total=len(loader)):
-        data = data.to(device)
         optimizer.zero_grad()
-
         loss = loss_fn(model=model,
-                       batch=data)
-
+                       batch=data.to(device))
         loss.backward()
         optimizer.step()
-        loss_tot += loss.item()
-        #base_tot += (score ** 2 / score_norm).mean().item()
-
+        loss_tot += loss.detach().item()
+        del loss
     loss_avg = loss_tot / len(loader)
-    base_avg = base_tot / len(loader)
-    return loss_avg, base_avg
+    return loss_avg
 
 
 @torch.no_grad()
 def test_epoch(loss_fn, model, loader, device):
     loss_tot = 0
-    base_tot = 0
     model.eval()
     for data in tqdm(loader, total=len(loader)):
-
         loss = loss_fn(model=model,
-                       batch=data)
-
+                       batch=data.to(device))
         loss_tot += loss.item()
-        #base_tot += (score ** 2 / score_norm).mean().item()
-
+        del loss
     loss_avg = loss_tot / len(loader)
-    base_avg = base_tot / len(loader)
-    return loss_avg, base_avg
+    return loss_avg
 
 
 
@@ -55,7 +44,7 @@ def get_ddpm_loss_fn(sampler, dataset, model, batch, train, reduce_mean=True):
   perb_dist, noise = sampler.q_sample(batch.cg_dist, t)
   batch.cg_perb_dist = perb_dist
   batch = dataset.reverse_coarse_grain(batch, batch_size=batch_size)
-  score = model(batch)
+  score, _ = model(batch)
   losses = torch.square(score - noise)
   losses = reduce_op(losses.reshape(losses.shape[0], -1), dim=-1)
   loss = torch.mean(losses)
