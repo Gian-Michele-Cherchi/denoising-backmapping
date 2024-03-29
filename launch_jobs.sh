990e09ade59f160b9f742eaecb09ee5080442fd0
Gian-Michele <giovanni_michel.cherchi@uca.fr>
2024-03-29 19:51:51 +0100

    job script add

---
+++ launch_jobs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=denoising_backmapping
+#SBATCH --time=14-00:00:00
+#SBATCH --partition=simatlab-gpu
+#SBATCH --ntasks=4
+#SBATCH --nodes=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=giovanni_michel.cherchi@uca.fr
+#SBATCH --gres=gpu:a100:1
+
+
+#poetry run python src/train.py train=diffusion
