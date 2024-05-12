03ecb89fe39373cbae020ece22d937e44cb1894a
Gian-Michele-Cherchi <g.m.cherchi@gmail.com>
2024-05-12 23:18:24 +0200

    minor updates

--- src/train.py
+++ src/train.py
@@ -12,34 +12,36 @@ def train_app(cfg: DictConfig) -> None:
     # log to wandb and  get train config
     run = wandb.init(
         project="denoising-backmapping",
-        config=cfg.train.__dict__['_content']
+        config=cfg.train.__dict__['_content'],
+        #resume="allow"
     )
     diffusion_config = cfg.train.diffusion
     model_config = cfg.train.model
     data_config = cfg.train.data
     train_config = cfg.train.train
     
-    ddp=False
+    ddp = False 
     device = 'gpu'
     #CPU/GPU Multiprocessing spawn process 
     
     world_size = torch.cuda.device_count() if device == 'gpu' else 2
     if ddp:
         mp.spawn(run_process, args=(
+            ddp,
             world_size, 
             device,
             train_config, 
             diffusion_config,
             data_config,
             model_config, 
-            run,
-            ), 
+            run), 
             nprocs=world_size, 
             join=True
                 )
     else:
         gpu_id = train_config.gpu_id
-        run_process(gpu_id,ddp, 
+        run_process(gpu_id,
+                    ddp, 
                     world_size, 
                     device, 
                     train_config, 
